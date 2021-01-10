f532827a4fdd13f05ec736f22ecaf423890b18fc
Nacho Cougil <nacho.cougil@dynatrace.com>
2021-01-10 23:34:46 +0100

    Refactor for better understanding

--- src/main/java/com/cougil/StringCalculator.java
+++ src/main/java/com/cougil/StringCalculator.java
@@ -11,8 +11,36 @@ public class StringCalculator {
     private static final String DEFAULT_DELIMITER = ",";
 
     public static int add(String text) {
+        String delimiter = calculateDelimiter(text);
+        if (!delimiter.equals(DEFAULT_DELIMITER)) {
+            text = text.substring(text.indexOf("\n") +1);
+        }
+        final String[] numbers = text.split("[\\s"+delimiter+"]+");
         List<Integer> negatives = new ArrayList<>();
+        final int result = calculateResult(numbers, negatives);
+        checkNegatives(negatives);
+        return result;
+    }
 
+    private static void checkNegatives(List<Integer> negatives) {
+        final String invalidNumbers = negatives.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        if (invalidNumbers.length() > 0) throw new NumberFormatException("Negatives are not allowed. Found: "+invalidNumbers);
+    }
+
+    private static int calculateResult(String[] numbers, List<Integer> negatives) {
+        return Arrays.stream(numbers)
+                .filter(string -> string.length() > 0)
+                .mapToInt(Integer::parseInt)
+                .peek(number -> {
+                    if (number < 0) negatives.add(number);
+                })
+                .filter( number -> number <= 1000 )
+                .sum();
+    }
+
+    private static String calculateDelimiter(String text) {
         String delimiter = DEFAULT_DELIMITER;
         int pos = text.indexOf("\n");
         if (pos > -1 && text.startsWith(PREFIX)) {
@@ -21,21 +49,7 @@ public class StringCalculator {
                 delimiter = delimiter.replaceAll("\\[", "");
                 delimiter = delimiter.replaceAll("]","");
             }
-            text = text.substring(pos+1);
         }
-        final String[] numbers = text.split("[\\s"+delimiter+"]+");
-        final int result = Arrays.stream(numbers)
-                .filter(string -> string.length() > 0)
-                .mapToInt(Integer::parseInt)
-                .peek(number -> {
-                    if (number < 0) negatives.add(number);
-                })
-                .filter( number -> number <= 1000 )
-                .sum();
-        final String invalidNumbers = negatives.stream()
-                .map(Object::toString)
-                .collect(Collectors.joining(","));
-        if (invalidNumbers.length() > 0) throw new NumberFormatException("Negatives are not allowed. Found: "+invalidNumbers);
-        return result;
+        return delimiter;
     }
 }
